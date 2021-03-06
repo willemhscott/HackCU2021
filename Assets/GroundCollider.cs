a23d28c53b304cf2f7bebfa2957c2ca6d74f1560
henry232323 <14969260+henry232323@users.noreply.github.com>
2021-03-06 14:35:33 -0700

    wow apparently all doesnt mean all

---
+++ Assets/GroundCollider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class GroundCollider : MonoBehaviour
+{
+    public PlayerMovement movement;
+    private void OnCollisionEnter(Collision collision)
+    {
+        movement.velocity.y = 0;
+        movement.gravity.y = 0;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        movement.gravity.y = -9.81f;
+    }
+}
